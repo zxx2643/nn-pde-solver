211c4dbe827c230cf954ce504bc719c94af1772a
zhangxx2643 <zhangxx2643@gmail.com>
2022-05-10 11:33:05 -0400

    control gpu memory growing for tf

--- src/Example1_diffusion_steady_state/main.py
+++ src/Example1_diffusion_steady_state/main.py
@@ -1,5 +1,12 @@
 import sys
 sys.path.append('../')
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+  tf.config.experimental.set_memory_growth(physical_devices[0], True)
+except:
+  # Invalid device or cannot modify virtual devices once initialized.
+  pass
 from pde_system_diffusion_steady_state import WeakPDESteadyStateDiffusion as thisPDESystem
 
 if __name__ == '__main__':
